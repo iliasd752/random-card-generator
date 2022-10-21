ca78cf91a58ba94182f299b8b1b86bd6756432bc
Ilias Dinia <iliasd752@gmail.com>
2022-10-21 17:44:59 +0000

    Random card generator completed but colors not working

--- src/app.js
+++ src/app.js
@@ -7,5 +7,61 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function randomDeck() {
+    const number = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    const symbol = ["Hearts", "Spades", "Diamonds", "Clubs"];
+
+    const cards = [];
+    for (let i = 0; i < number.length; i++) {
+      for (let s = 0; s < symbol.length; s++) {
+        const value = number[i];
+        const sign = symbol[s];
+        cards.push({ value, sign });
+      }
+    }
+    return cards;
+  }
+  console.log(randomDeck());
+
+  function randomCard(cards) {
+    const random = Math.floor(Math.random() * 51);
+    const cardValue = cards[random].value;
+    const cardSign = cards[random].sign;
+    let entity;
+    cardSign === "Diamonds"
+      ? (entity = "&diams;")
+      : (entity = "&" + cardSign.toLowerCase() + ";");
+    const card = document.createElement("div");
+    card.classList.add("card", cardValue.toLowerCase());
+    card.innerHTML =
+      '<span class="card-value-suit top">' +
+      cardValue +
+      entity +
+      "</span>" +
+      '<span class="card-suit">' +
+      entity +
+      "</span>" +
+      '<span class="card-value-suit bot">' +
+      cardValue +
+      entity +
+      "</span>";
+    document.body.appendChild(card);
+  }
+  const cards = randomDeck();
+  randomCard(cards);
 };
