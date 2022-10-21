3099696d98b3ccdb24fbe57ed745692d01eaa82a
Ilias Dinia <iliasd752@gmail.com>
2022-10-21 18:32:49 +0000

    Added a button to generate a new card

--- src/app.js
+++ src/app.js
@@ -64,4 +64,11 @@ window.onload = function() {
   }
   const cards = randomDeck();
   randomCard(cards);
+  const refreshButton = document.querySelector(".refresh-button");
+
+  const refreshPage = () => {
+    location.reload();
+  };
+
+  refreshButton.addEventListener("click", refreshPage);
 };
